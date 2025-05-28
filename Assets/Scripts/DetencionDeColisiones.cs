e282f1013d70b1e730bb418077b79162e4a63dfc
Matikuper <167240569+Matikuper@users.noreply.github.com>
2025-05-28 15:48:09 -0300

    Contacto con barril
    AGUANTE LA ACADEMIA

---
+++ Assets/Scripts/DetencionDeColisiones.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetencionDeColisiones : MonoBehaviour
+{
+
+void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log($"Contacto con {collision.gameObject.name}");
+        Destroy(collision.gameObject);
+    }
+}
---
+++ Assets/Scripts/MercanciaScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+    public int precio;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log($"Contacto con {collision.gameObject.name}");
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
